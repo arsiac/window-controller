2475af83a76dcceb410372ec50041143ae638439
arsiac <arsia.chou@gmail.com>
2020-08-14 20:25:24 +0800

    更新pack

--- pack.bat
+++ pack.bat
@@ -5,4 +5,4 @@ md .\Release\window-controller\include\
 
 copy .\window-controller\*.h .\Release\window-controller\include\
 copy .\Release\window-controller.lib .\Release\window-controller\
-copy .\window-controller.a .\Release\window-controller\
+copy .\libwindow-controller.a .\Release\window-controller\
